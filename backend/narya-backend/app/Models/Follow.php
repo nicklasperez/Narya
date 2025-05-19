c3d284aa36dbff6f091dee9d8a8b494d062e2ebe
Nick <nicklasperez@digitechfp.com>
2025-05-19 20:55:02 +0200

    last changes

---
+++ backend/narya-backend/app/Models/Follow.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Follow extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'follower_id',
+        'followed_id',
+    ];
+
+    /**
+     * Relación con el usuario que sigue.
+     */
+    public function follower(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
+
+    /**
+     * Relación con el usuario seguido.
+     */
+    public function followed(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'followed_id');
+    }
+}
---
+++ backend/narya-backend/app/Models/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'spotify_id',
+        'spotify_access_token',
+        'spotify_refresh_token',
+        'spotify_token_expires_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'spotify_access_token',
+        'spotify_refresh_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'spotify_token_expires_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Get the songs associated with the user.
+     */
+    public function songs(): HasMany
+    {
+        return $this->hasMany(Song::class);
+    }
+
+    /**
+     * Get the followers of the user.
+     */
+    public function followers(): HasMany
+    {
+        return $this->hasMany(Follow::class, 'followed_id');
+    }
+
+    /**
+     * Get the users the user is following.
+     */
+    public function following(): HasMany
+    {
+        return $this->hasMany(Follow::class, 'follower_id');
+    }
+}
